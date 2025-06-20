af4e8c9c5a2f0ae52d426ddf741ba783f1e99773
Ilia Grigorev <grigorieviv@gmail.com>
2025-06-20 12:13:01 +0300

    Добавлена отправка сообщений в notification-service
    Добавлена отправка сообщений через Kafka в notification-service по событиям создания и удаления пользователя. Сообщения содержат email-адрес, subject, body для последующей отправки письма на почту

---
+++ src/main/java/ru/astondevs/config/KafkaProducerConfig.java
@@ -0,0 +1,42 @@
+package ru.astondevs.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServers;
+
+    @Value("${kafka.topic.notification}")
+    private String topicName;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic notificationTopic() {
+        return new NewTopic(topicName, 1, (short) 1);
+    }
+}
